335fde4410c175bf7cb9e4fee555c200d4ecfee1
MatheusMarinho57 <matheusmarinho57@gmail.com>
2024-08-18 12:34:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const textArea = document.querySelector('#typehere');
+const output = document.querySelector('#output-area');
+const criptograph = document.querySelector('#cript');
+const descriptograph = document.querySelector('#descrip');
+
+const btnCriptograph = () => {
+  const value = criptograph.value;
+  
+}
